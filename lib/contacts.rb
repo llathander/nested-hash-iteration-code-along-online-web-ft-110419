8741da21154e955ab93664f0c9139f13515dea08
Adrian Samaniego <llathander@gmail.com>
2019-12-01 00:42:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,11 +16,11 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
+      if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-    binding.pry
+      binding.pry
+      end
     end
   end
-end
 end
 
